80da61a457bcbf7cd9928c1c345f31a0cd25f2ac
Lucca <lucca_uel@outlook.com>
2025-03-19 11:46:57 -0300

    first commit

---
+++ app/Models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    public function up(){
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+}
